a6603c02baeec73c8af42fcb4d169da33649df6c
evanhzg <evan.hoizey@ynov.com>
2024-10-10 10:16:32 +0200

    design fixes

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -77,7 +77,7 @@ export default async function InvoicesTable({
                 </th>
               </tr>
             </thead>
-            <tbody className="bg-white">
+            <tbody className="divide-y divide-gray-200 bg-white text-gray-900">
               {invoices?.map((invoice) => (
                 <tr
                   key={invoice.id}
